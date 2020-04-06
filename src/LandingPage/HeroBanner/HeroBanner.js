389d583a0b1a115a91327bde498a7be4ec64aaa7
Panos Hapsides <p.hapsides@live.com>
2020-04-06 17:23:50 +0100

    Componetise the banner

---
+++ src/LandingPage/HeroBanner/HeroBanner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HeroBanner.css';
+
+export default function HeroBanner(props) {
+    return (
+        <div className="HeroBanner">
+             <div className="hero-overlay text-center">
+                <h1>{props.title}</h1>
+                <button class="btn btn-transparent rounded-capsule">
+                    {props.buttonLabel}
+                </button>
+            </div>
+            <img 
+                src={props.image} 
+                alt={props.imageAlt} 
+                className="hero-image overlay"
+            />
+        </div>
+    );
+}
