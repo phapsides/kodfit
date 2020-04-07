3d5e8e027c12f647db517df0c330877822cb1a33
marianmolocea <51676128+marianmolocea@users.noreply.github.com>
2020-04-07 14:53:39 +0100

    Change file structure (#17)
    * Create bottom navigation
    
    * Add general styling box-sizing
    
    * Create a new data structure for the button and iterate through
    
    * install react-google-maps/api
    
    * Merge from Upstream
    
    * Rename the components in NavBar file structure.
    
    * Resturcture the pages into Page view Component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import BottomNavBar from './BottomNavBar/BottomNavBar';
 import './App.css';
 import './Utility.css';
 import './Base.css';
-import CalendarApp from './CalendarApp/CalendarApp';
-import NearMeGyms from './NearMe/NearMeGyms/NearMeGyms';
-import NearMeTrainers from './NearMe/NearMeTrainers/NearMeTrainers';
-import WorkoutsMain from './WorkoutsMain/WorkoutsMain';
-import WorkoutList from './WorkoutsMain/WorkoutList/WorkoutList';
-import WorkoutCurrent from './WorkoutsMain/WorkoutCurrent/WorkoutCurrent';
-import NearMe from './NearMe/NearMe';
-import Tracker from './Tracker/Tracker';
-import Login from './Login/Login';
-import Timer from './Timer/Timer';
+import PageView from './PageView/PageView';
+import LandingPage from './LandingPage/LandingPage';
+
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
 				<h1>Kodfit</h1>
-				<Route path="/calendar" exact component={CalendarApp} />
-				<Route path="/near-me" exact component={NearMe} />
-				<Route path="/near-me-gyms" exact component={NearMeGyms} />
-				<Route path="/near-me-trainers" exact component={NearMeTrainers} />
-				<Route path="/workout-main" exact component={WorkoutsMain} />
-				<Route path="/workout-list" exact component={WorkoutList} />
-				<Route path="/workout-current" exact component={WorkoutCurrent} />
-				<Route path="/tracker" exact component={Tracker} />
-				<Route path="/login" exact component={Login} />
-				<Route path="/timer" exact component={Timer} />
-				<BottomNavBar />
+				<PageView />
+				<Route path="/" exact component={LandingPage} />
 			</div>
 		</Router>
 	);
