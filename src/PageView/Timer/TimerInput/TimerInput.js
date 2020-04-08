596d518d883269010dd8bcf069bebb28838bd522
ebrimaconta <ebrimaconta@gmail.com>
2020-04-08 17:53:11 +0100

    Added Hours and a  reset button (#24)
    * Added hours
    
    * Added hours
    
    * Added hours

--- src/PageView/Timer/TimerInput/TimerInput.js
+++ src/PageView/Timer/TimerInput/TimerInput.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default function TimerInput({field, name}) {
+export default function TimerInput({field, name,value}) {
     return (
         <input
              type='number'
              name={name}
              min='0'
              max='60'
+             value={value}
              onChange={field}
         />
     )
