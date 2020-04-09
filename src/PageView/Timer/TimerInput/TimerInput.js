eb9cd9180b380310c89e9a7a56f34f8c8756786a
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-04-09 20:23:43 +0100

    refactor: Remove unnecessary component

--- src/PageView/Timer/TimerInput/TimerInput.js
+++ src/PageView/Timer/TimerInput/TimerInput.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default function TimerInput({field, name}) {
+export default function TimerInput({field}) {
     return (
         <input
              type='number'
-             name={name}
              min='0'
              max='60'
              onChange={field}
