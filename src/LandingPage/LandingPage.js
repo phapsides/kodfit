4c2030ead558f2f8a2da88004bd3014beb80b739
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-04-11 16:11:34 +0100

    chore: Removing unnecessary files

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,12 +4,10 @@ import HeroBanner from './HeroBanner/HeroBanner';
 import heroImage from './../common/images/hero-image.jpg';
 import about from './../common/images/about.jpg';
 import ourGoal from './../common/images/our-goal.jpg';
-// import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function LandingPage() {
     return (
         <main className="LandingPage">
-            {/* <Router> */}
             <HeroBanner 
                 title="Train & Feel Better Together :)" 
                 image={heroImage}
@@ -31,7 +29,6 @@ export default function LandingPage() {
                 buttonLabel="Read More"
                 buttonLink="/our-goal"
             />
-            {/* </Router> */}
         </main>
     );
 }
--- src/LandingPage/OurGoal/OurGoal.js
+++ src/LandingPage/OurGoal/OurGoal.js
@@ -65,16 +65,3 @@ export default function OurGoal() {
     </div>
   );
 }
-
-// eslint-disable-next-line
-{
-  /* <img src={motivational} alt="Motivational" /> */
-  /* <img src={jogger} alt="Joger" /> */
-  // import motivational from "../../common/images/ourGoal-motivational.jpg";
-  // import swimmer from "../../common/images/ourGoal-swimming.jpg";
-  /* <p>
-We want to make fitness easy. It fits in with your schedule you needn't
-travel far. Our goal is to prove that a healthy you is right around the
-corner.
-</p> */
-}
