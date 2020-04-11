dec1e58cbae29b231cfc75829628784289e4e485
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-04-11 14:04:16 +0100

    feat: Make OurGoals desktop-friendly

--- src/LandingPage/OurGoal/OurGoal.js
+++ src/LandingPage/OurGoal/OurGoal.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./OurGoal.css";
-// import motivational from "../../common/images/ourGoal-motivational.jpg";
+import "animate.css";
+import "hover.css";
+import { Link } from "react-router-dom";
 import jogger from "../../common/images/female-jogger.jpg";
-// import swimmer from "../../common/images/ourGoal-swimming.jpg";
 import { FaDumbbell, FaChartLine, FaHeartbeat } from "react-icons/fa";
-import 'animate.css';
-import 'hover.css';
 
 export default function OurGoal() {
   return (
     <div className="OurGoal">
       <h1>Our Goal</h1>
       <hr />
-      <p>
+      <p className="subtext">
         Our goal is to put you in touch with gyms and personal trainers nearby.
         In the age of Kodfit, winning is easier than ever before.
       </p>
@@ -23,31 +22,46 @@ export default function OurGoal() {
             <div className="icon animated fadeInLeft delay-1s">
               <FaDumbbell />
             </div>
-            <p className="animated fadeInRight delay-1s">...Put you in touch with gyms and trainers nearby</p>
+            <p className="slidingText animated fadeInRight delay-1s">
+              ...Put you in touch with gyms and trainers nearby
+            </p>
           </div>
           <div className="iconAndText">
             <div className="icon animated fadeInLeft delay-2s">
               <FaHeartbeat />
             </div>
-            <p className="animated fadeInRight delay-2s">...Deliver you a workout routine tailored to your goals</p>
+            <p className="slidingText animated fadeInRight delay-2s">
+              ...Deliver you a workout routine tailored to your goals
+            </p>
           </div>
           <div className="iconAndText">
             <div className="icon animated fadeInLeft delay-3s">
               {/* <ProgressIcon /> */}
               <FaChartLine />
             </div>
-            <p className="animated fadeInRight delay-3s">...Keep track of your progress as you get healthier</p>
+            <p className="slidingText animated fadeInRight delay-3s">
+              ...Keep track of your progress as you get healthier
+            </p>
           </div>
         </div>
         <img className="image" src={jogger} alt="Jogger" />
       </div>
-
+    
       <p>
-        {" "}
-        The fitter, stronger you is closer than you think! (Link to Nearby Gyms)
+        The fitter, stronger you is closer than you think
       </p>
-      <button className="hvr-shutter-out-horizontal">Find your nearest gyms!</button>
-      <button className="hvr-shutter-out-horizontal">Find your nearest personal trainer!</button>
+      <Link to="/near-me-gyms">
+        <button className="hvr-shutter-out-horizontal">
+          Find your nearest gyms!
+        </button>
+      </Link>
+      <Link to="/near-me-trainers">
+
+        <button className="hvr-shutter-out-horizontal">
+          Find your nearest personal trainer!
+        </button>
+      </Link>
+   
     </div>
   );
 }
@@ -56,6 +70,8 @@ export default function OurGoal() {
 {
   /* <img src={motivational} alt="Motivational" /> */
   /* <img src={jogger} alt="Joger" /> */
+  // import motivational from "../../common/images/ourGoal-motivational.jpg";
+  // import swimmer from "../../common/images/ourGoal-swimming.jpg";
   /* <p>
 We want to make fitness easy. It fits in with your schedule you needn't
 travel far. Our goal is to prove that a healthy you is right around the
