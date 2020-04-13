fa1bbe9ee8644a2ad8eca7d62d2526dd55e4c531
Darango84 <56847253+Darango84@users.noreply.github.com>
2020-04-13 16:41:29 +0100

    commit to fork pre merge with upstream (#34)
    Co-authored-by: Daniel Arango <danielarango@daniels-air.lan>

--- src/PageView/WorkoutsMain/WorkoutList/WorkoutList.js
+++ src/PageView/WorkoutsMain/WorkoutList/WorkoutList.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import './WorkoutList.css';
+import listChest from '../../../common/images/listChest.png';
+import listBack from '../../../common/images/listBack.png';
+import listLegs from '../../../common/images/listLegs.png';
+import listShoulders from '../../../common/images/listShoulders.png';
+import listArms from '../../../common/images/listArms.png';
+import listCardio from '../../../common/images/listCardio.png';
 
 export default function WorkoutList() {
     return (
-        <div className="WorkoutList">
-            <h2 className="WorkoutListTitle">Exercise List</h2>
-            <main className="container">
-                <div className="container-content">Exercise list A</div>
-                <div className="container-content">Exercise list B</div>
-                <div className="container-content">Exercise list C</div>
-                <div className="container-content">Exercise list D</div>
-                <div className="container-content">Exercise list E</div>
-                <div className="container-content">Exercise list F</div>
+        <div className="WorkoutsList">
+            <div className="containerTop">
+            <a className="button One" href="./Home">Workouts</a>
+            <a className="button Two" href="./workout-list">Exercise List</a>
+            </div>
+            
+            <main className="containerBottom">
+                <div className="list">
+                    <img src={listChest} alt="list one"/>
+                    <div className="text">Chest</div>
+                </div>
+                <div className="list">
+                    <img src={listBack} alt="list two"/>
+                    <div className="text">Back</div>
+                </div>
+                <div className="list">
+                    <img src={listLegs} alt="list three"/>
+                    <div className="text">Legs</div>
+                </div>
+                <div className="list">
+                    <img src={listShoulders} alt="list four"/>
+                    <div className="text">Shoulders</div>
+                </div>
+                <div className="list">
+                    <img src={listArms} alt="list five"/>
+                    <div className="text">Arms</div>
+                </div>
+                <div className="list">
+                    <img src={listCardio} alt="list six"/>
+                    <div className="text">cardio</div>
+                </div>
+                
+                
+                
+                
             </main>
         </div>
     )
--- src/PageView/WorkoutsMain/WorkoutsMain.js
+++ src/PageView/WorkoutsMain/WorkoutsMain.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import './WorkoutsMain.css';
+import workoutAbs from '../../common/images/workoutAbs.png';
+import workoutLegs from '../../common/images/workoutLegs.png';
+import workoutChest from '../../common/images/workoutChest.png';
+import workoutShoulders from '../../common/images/workoutShoulders.png';
+import workoutCalisthenics from '../../common/images/workoutCalisthenics.png';
+import workoutArms from '../../common/images/workoutArms.png';
 
 export default function WorkoutsMain() {
     return (
         <div className="WorkoutsMain">
-            <main className="container">
-                <div className="container-content">Workout Routine</div>
-                <div className="container-content">Workout Routine</div>
-                <div className="container-content">Workout Routine</div>
-                <div className="container-content">Workout Routine</div>
-                <div className="container-content">Workout Routine</div>
-                <div className="container-content">Workout Routine</div>
+            <div className="containerTop">
+            <a className="button One" href="./Home">Workouts</a>
+            <a className="button Two" href="./workout-list">Exercise List</a>
+            </div>
+            
+            <main className="containerBottom">
+                <div className="routine">
+                    <img src={workoutAbs} alt="routine one"/>
+                    <div className="text">Killer Core & Abs</div>
+                </div>
+                <div className="routine">
+                    <img src={workoutLegs} alt="routine two"/>
+                    <div className="text">Legs & cardio</div>
+                </div>
+                <div className="routine">
+                    <img src={workoutChest} alt="routine three"/>
+                    <div className="text">Chest Day!</div>
+                </div>
+                <div className="routine">
+                    <img src={workoutShoulders} alt="routine four"/>
+                    <div className="text">Shoulders challenge</div>
+                </div>
+                <div className="routine">
+                    <img src={workoutCalisthenics} alt="routine five"/>
+                    <div className="text">Calisthenics</div>
+                </div>
+                <div className="routine">
+                    <img src={workoutArms} alt="routine six"/>
+                    <div className="text">Biceps & Triceps</div>
+                </div>
+                
+                
+                
+                
             </main>
         </div>
     )
