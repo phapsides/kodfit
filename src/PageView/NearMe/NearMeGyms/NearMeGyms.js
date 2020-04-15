cda74abdb60a5094f87af0485e92556d102c5925
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-04-15 15:23:22 +0100

    feat: Make component desktop friendly with buttons

--- src/PageView/NearMe/NearMeGyms/NearMeGyms.js
+++ src/PageView/NearMe/NearMeGyms/NearMeGyms.js
@@ -1,18 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import {
-  GoogleMap,
-  LoadScript,
-  Marker,
-} from '@react-google-maps/api';
-import './NearMeGyms.css';
-import InfoWindows from './InfoWindows/InfoWindows'
+import React, { useState, useEffect } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import "./NearMeGyms.css";
+import InfoWindows from "./InfoWindows/InfoWindows";
 
 export default function NearMeGyms() {
   const [data, setData] = useState([]);
   const [location, setLocation] = useState({ lat: 51.5007, lng: -0.1246 });
-  const [markerId, setMarkerId] = useState('')
-  const [query, setQuery] = useState('gym')
-  const [title, setTitle] = useState('Gyms')
+  const [markerId, setMarkerId] = useState("");
+  const [query, setQuery] = useState("gym");
+  const [title, setTitle] = useState("Gyms");
 
   useEffect(() => {
     let successPosition = (position) => {
@@ -20,7 +16,7 @@ export default function NearMeGyms() {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });
-    }
+    };
     let errorHandle = () => setLocation({ lat: 51.5007, lng: -0.1246 });
     navigator.geolocation.getCurrentPosition(successPosition, errorHandle);
   }, []);
@@ -38,13 +34,16 @@ export default function NearMeGyms() {
   const updateQuery = (query, title) => {
     setQuery(query);
     setTitle(title);
-  }
+  };
 
   return (
     <div className="NearMeGyms">
       <div className="containerOne">
         <div className="map containerOne-content">
-          <LoadScript id="load-script" googleMapsApiKey='AIzaSyAHs3RiKCGgJhll5xXKjLFqprAi6-LW3Rc'>
+          <LoadScript
+            id="load-script"
+            googleMapsApiKey="AIzaSyAHs3RiKCGgJhll5xXKjLFqprAi6-LW3Rc"
+          >
             <GoogleMap
               id="gmap"
               zoom={13}
@@ -56,34 +55,45 @@ export default function NearMeGyms() {
                   <Marker
                     key={place.id}
                     position={place.position}
-                    onClick={() => setMarkerId(place.id)}>
-                      {
-                        markerId === place.id ? <InfoWindows place={place} /> : ''
-                      }                                         
-                    </Marker>
+                    onClick={() => setMarkerId(place.id)}
+                  >
+                    {markerId === place.id ? <InfoWindows place={place} /> : ""}
+                  </Marker>
                 ))}
             </GoogleMap>
           </LoadScript>
         </div>
 
-<div className="switch-buttons">
-                      <button className="toggleQuery" onClick={() => updateQuery('gym', 'Gyms')}>Find your nearest gym</button>
-                      <button className="toggleQuery" onClick={() => updateQuery('personal+trainer', 'Personal Trainers')}>Find your nearest personal trainer</button>
-</div>
-
-        <div className="list containerOne-content">
-                    <h3 className="listTitle">List of {title}</h3>
-          <ul className="listContent">
-            {
-              data && data.map((gym, index) => (
-              <li key="gym.title" className="listContent-item">
-                {index + 1}. {gym.title}
-                <br />
-                Distance: {gym.distance}m
-              </li>
-              ))
-            }
-          </ul>
+        <div className="list-and-buttons">
+          <div className="switch-buttons">
+            <button
+              className="toggleQuery"
+              onClick={() => updateQuery("gym", "Gyms")}
+            >
+              Find your nearest gym
+            </button>
+            <button
+              className="toggleQuery"
+              onClick={() =>
+                updateQuery("personal+trainer", "Personal Trainers")
+              }
+            >
+              Find your nearest personal trainer
+            </button>
+          </div>
+          <div className="list containerOne-content">
+            <h3 className="listTitle">List of {title}</h3>
+            <ul className="listContent">
+              {data &&
+                data.map((gym, index) => (
+                  <li key="gym.title" className="listContent-item">
+                    {index + 1}. {gym.title}
+                    <br />
+                    Distance: {gym.distance}m
+                  </li>
+                ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
