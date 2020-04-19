1a23a905c8ef269aefb749c9c421ec7ee680dc27
Adam Zdrzalka <Adam.JZ@outlook.com>
2020-04-19 18:35:34 +0100

    feat: Move gyms, trainers to single root component

--- src/PageView/PageView.js
+++ src/PageView/PageView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CalendarApp from './CalendarApp/CalendarApp';
-import NearMeGyms from './NearMe/NearMeGyms/NearMeGyms';
+import NearMe from './NearMe/NearMe';
 import WorkoutsMain from './WorkoutsMain/WorkoutsMain';
 import WorkoutList from './WorkoutsMain/WorkoutList/WorkoutList';
 import ExerciseListDetails from './WorkoutsMain/WorkoutExerciseList/ExerciseListDetails';
@@ -27,7 +27,7 @@ export default function PageView() {
         <Route path="/about" exact component={AboutUs} />
         <Route path="/our-goal" exact component={OurGoal} />
         <Route path="/dashboard/calendar" exact component={CalendarApp} />
-        <Route path="/dashboard/near-me-gyms" exact component={NearMeGyms} />
+        <Route path="/dashboard/near-me" exact component={NearMe} />
         <Route path="/dashboard/home" exact component={WorkoutsMain} />
         <Route path="/dashboard/workout-list" exact component={WorkoutList} />
         <Route path="/dashboard/workout-list/:listId" exact component={ExerciseListDetails} />
