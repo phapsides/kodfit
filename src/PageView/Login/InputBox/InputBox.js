38bfbded48774e7c70a28cd715402ce32f76ad9a
Casey Minji Jeong <58687428+casey-minzi@users.noreply.github.com>
2020-04-23 16:20:48 +0100

    Applied style guide , break into sub components (#53)
    * Created basic skeleton of pages / login page WIP
    
    * Added semicolon & minor changes as suggested on the PR
    
    * Added Route / deleted App.css text align:center
    
    * installed react icons, minor sementic change
    
    * fixing modules issue
    
    * embeded header logo / centered login input
    
    * changed original of small logo
    
    * installed react-google-login
    
    * testing google login
    
    * Created skeleton of Profile page
    
    * Added profile url to navbar
    
    * testing userdata passing between components
    
    * using context API to pass data (test)
    
    * sending data through context API (still in progress)
    
    * fixing conflict
    
    * changes for PR
    
    * fixed typo
    
    * using local storage to store user data
    
    * changed route path for profile component & sementic change
    
    * centered login input & styled buttons
    
    * added background img to profile
    
    * redirect to 'dashboard/home' after sign in
    
    * fixed typo
    
    * Created login button component
    
    * Change header size in profile component
    
    * applied style guide to login component
    
    * fixed redirect route in Login component according to upstream merge
    
    * adjusting profile component to desktop mode (still in progress)
    
    * Changed data in tracker
    
    * Added bar graph to profile component
    
    * Arranged Profile.js style in desktop mode
    
    * fixing margin in Profile.css
    
    * mock up login (abled login by typing in username)
    
    * added semicolon
    
    * applied requested change

--- src/PageView/Login/InputBox/InputBox.js
+++ src/PageView/Login/InputBox/InputBox.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import defaultUserImage from '../../../common/images/default-user-image.png';
 
-export default function InputBox({ type, placeholder }) {
+export default function InputBox({ type, placeholder, onSignIn }) {
+
+    function getUserName(value) {
+        setTimeout(() => {
+            localStorage.setItem('userName', value);
+            localStorage.setItem('userImageUrl', defaultUserImage);
+            onSignIn(value);
+        }, 3000);
+    }
     return (
         <div className="inputBox-content">
             <input
                 type={type}
                 placeholder={placeholder}
+                onChange={e => getUserName(e.target.value)}
             />
         </div>
     );
--- src/PageView/Profile/Profile.js
+++ src/PageView/Profile/Profile.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Profile.css';
+import PersonalInfo from './PersonalInfo/PersonalInfo';
+import { Bar } from 'react-chartjs-2'
+import { barOption, data } from '../Tracker/Data';
+import Goal from './Goal/Goal';
 
 export default function Profile() {
 
@@ -14,43 +18,17 @@ export default function Profile() {
                         <div className="profileBox-photo">
                             <img src={userImageUrl} alt={userName} />
                         </div>
-                        <h3 className="profileBox-userInfo">{userName}, 24</h3>
-                    </div>
-                    <div className="personalInfo">
-                        <div className="personalInfo-content">
-                            <span>weight</span>
-                            <span className="text-bold">50kg</span>
-                        </div>
-                        <div className="personalInfo-content">
-                            <span>height</span>
-                            <span className="text-bold">160 cm</span>
-                        </div>
-                        <div className="personalInfo-content">
-                            <span>bmi</span>
-                            <span className="text-bold">19.5</span>
-                        </div>
-                        <div className="personalInfo-content">
-                            <span>sex</span>
-                            <span className="text-bold">F</span>
-                        </div>
-                    </div>
-                    <div className="goal">
-                        <h4>Goal</h4>
-                        <div className="goal-weight">
-                            <span>weight</span>
-                            <span>48 kg</span>
-                        </div>
-                        <div className="goal-weight">
-                            <span>bmi</span>
-                            <span>18.7</span>
-                        </div>
-
+                        <h2 className="profileBox-userInfo">{userName}</h2>
+                        <PersonalInfo />
                     </div>
                 </div>
                 <div className="container-content">
+                    <Goal />
+                </div>
+                <div className="container-content">
                     <h3>Daily Workout Duration</h3>
-                    <div className="durationGraph">
-                        Here will be graph
+                    <div className="graph">
+                        <Bar data={data} options={barOption} />
                     </div>
                 </div>
             </div>
